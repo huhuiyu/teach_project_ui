a374730f0093a623fe51b7641aabcdafb435fd64
huhuiyu <1069306849@qq.com>
2022-12-26 09:14:50 +0800

    添加推送和拉取脚本

---
+++ push.ps1
@@ -0,0 +1,4 @@
+# github =====> git remote add github git@github.com:huhuiyu/teach_project_ui.git
+# jihulab =====> git remote add jihulab git@jihulab.com:DarkKnight/teach_project_ui.git
+git push
+git push jihulab
