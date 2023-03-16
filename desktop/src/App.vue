b56fc78e8a22f4e177b165ad866a3496d7c407fd
gaopeng <3011248542@qq.com>
2023-03-16 11:12:24 +0800

    全局ui设置中文

--- desktop/src/App.vue
+++ desktop/src/App.vue
@@ -1,16 +1,18 @@
 <template>
-  <n-dialog-provider>
-    <n-loading-bar-provider>
-      <n-notification-provider>
-        <n-message-provider>
-          <router-view></router-view>
-        </n-message-provider>
-      </n-notification-provider>
-    </n-loading-bar-provider>
-  </n-dialog-provider>
+  <n-config-provider :locale="locale" :date-locale="dateLocale">
+    <n-dialog-provider>
+      <n-loading-bar-provider>
+        <n-notification-provider>
+          <n-message-provider>
+            <router-view></router-view>
+          </n-message-provider>
+        </n-notification-provider>
+      </n-loading-bar-provider>
+    </n-dialog-provider>
+  </n-config-provider>
 </template>
 <script setup lang="ts">
-import { NDialogProvider, NLoadingBarProvider, NNotificationProvider, NMessageProvider } from 'naive-ui'
+import { NDialogProvider, NLoadingBarProvider, NNotificationProvider, NMessageProvider, zhCN, dateZhCN, NConfigProvider } from 'naive-ui'
 import store from './store'
 const storeInfo = store()
 storeInfo.queryCityInfo()
@@ -19,4 +21,7 @@ storeInfo.queryCountInfo()
 if (top != window) {
   top?.postMessage('loaded', '*')
 }
+
+const locale = zhCN
+const dateLocale = dateZhCN
 </script>
